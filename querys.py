e834836e055931af64ba3aff74e3c7b1c7232bea
Carlbom97 <simoncarlbom@outlook.com>
2023-09-13 09:09:38 +0200

    new

--- querys.py
+++ querys.py
@@ -22,7 +22,9 @@ delete_query = """
     DELETE FROM Games WHERE id > 32587;
 """
 
-call_db(delete_query)
+delete_db = "DROP TABLE IF EXISTS Expected_Games;"
+
+call_db(delete_db)
 
 # call_db(delete_dublicate)
 
